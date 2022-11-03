874d80c44932afcc1e1fb71a5b0a02ae64c15cbc
Glitch (login) <none>
2022-11-03 11:13:28 +0000

    👖📍 Checkpoint ./routes.js:26026384/480 ./controllers/accounts.js:26026384/72 ./views/index.hbs:26026384/24

--- routes.js
+++ routes.js
@@ -4,8 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 const start = require('./controllers/start');
-const bookmarks = require('./controllers/bookmarks.js');
-const booklist = require('./controllers/booklist.js');
 const about = require('./controllers/about.js');
 const accounts = require ('./controllers/accounts.js');
 
@@ -15,14 +13,6 @@ router.get('/signup', accounts.signup);
 router.get('/logout', accounts.logout);
 router.post('/register', accounts.register);
 router.post('/authenticate', accounts.authenticate);
-
-router.get('/bookmarks', bookmarks.index);
-router.get('/bookmarks/deletebooklist/:id', bookmarks.deleteBooklist);
-router.post('/bookmarks/addbooklist', bookmarks.addBooklist);
-
-router.get('/booklist/:id', booklist.index);
-router.get('/booklist/:id/deletebookmark/:bookmarkid', booklist.deleteBookmark);
-router.post('/booklist/:id/addbookmark', booklist.addBookmark);
 
 router.get('/about', about.index);
 
